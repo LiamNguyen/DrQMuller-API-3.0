562ea8570a3d9953e40bf0c30bc1ed3d12481662
Liam Nguyen <phuc.nguyen2@metropolia.fi>
2018-04-05 10:43:05 +0300

    Create https

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
+
 const database = require('./database');
 const appConfig = require('./config/appConfig');
 const routes = require('./routes');
+
+const sslkey = fs.readFileSync('./certificates/ssl-key.pem');
+const sslcert = fs.readFileSync('./certificates/ssl-cert.pem');
+
+const options = { key: sslkey, cert: sslcert };
 
 const app = express();
 
@@ -10,7 +18,7 @@ database.connect();
 appConfig(app);
 routes(app);
 
-module.exports.server = app.listen(5000);
+module.exports.server = https.createServer(options, app).listen(5000);
 if (process.env.NODE_ENV === 'development') {
   console.log('Listening on port 5000...');
 }
