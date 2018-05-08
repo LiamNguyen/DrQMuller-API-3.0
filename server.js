243861355aa395ece7da184c7160f8477aec581d
Liam Nguyen <phuc.nguyen2@metropolia.fi>
2018-05-09 00:39:07 +0300

    Add Profile and adjust server.js for heroku deployment

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
 
 const database = require('./database');
 const serverConfig = require('./config/serverConfig');
 const routes = require('./routes');
 const SocketIO = require('./SocketIO');
-
-const sslkey = fs.readFileSync('./certificates/ssl-key.pem');
-const sslcert = fs.readFileSync('./certificates/ssl-cert.pem');
-
-const options = { key: sslkey, cert: sslcert };
 
 const app = express();
 
@@ -24,7 +17,7 @@ let server;
 if (process.env.NODE_ENV === 'test') {
   server = app.listen(8081);
 } else {
-  server = https.createServer(options, app).listen(5000);
+  server = app.listen(process.env.PORT || 5000);
   if (process.env.NODE_ENV === 'development') {
     // Serve at this port only for raml specs
     app.listen(8080);
